36ae44c869426d51bb342a456eb9e4bc848696c9
lalyod <mpaproject307@gmail.com>
2023-10-08 20:54:24 +0700

    feat: make component sidebar for layout

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,34 @@
+<header style="background-color: #F2FAF1;" class="h-screen w-60 shadow">
+    <div class="px-8 pt-10">
+        <h3 class="font-bold text-center text-2xl">Pradita</h3>
+        <nav class="mt-8">
+            <ul class="font-bold flex flex-col gap-5">
+                <li>
+                    <a href="" class="flex gap-4 items-center"><img src="{{ asset('/img/home.svg') }}"
+                            class="bg-green p-3 rounded-lg shadow" alt=""><span>Beranda</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="flex gap-4 items-center"><img src="{{ asset('/img/heart.svg') }}"
+                            class="bg-white p-3 rounded-lg shadow" alt=""><span>Favorit</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="flex gap-4 items-center"><img src="{{ asset('/img/cart.svg') }}"
+                            class="bg-white p-3 rounded-lg shadow" alt=""><span>Keranjang</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="flex gap-4 items-center"><img src="{{ asset('/img/gear.svg') }}"
+                            class="bg-white p-3 rounded-lg shadow" alt=""><span>Pengaturan</span>
+                    </a>
+                </li>
+                <li class="mt-10">
+                    <a href="" class="flex gap-4 items-center"><img src="{{ asset('/img/exit.svg') }}"
+                            class="bg-white p-3 rounded-lg shadow" alt=""><span>Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
