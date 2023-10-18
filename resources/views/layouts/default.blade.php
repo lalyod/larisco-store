5d2cec73e5f3f9e43069e90f2d7fa91eeaf36af8
lalyod <mpaproject307@gmail.com>
2023-10-18 15:53:57 +0700

    feat: make dynamic title

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Larisco | {{ ucfirst($title) }}</title>
     @vite('resources/css/app.css')
 </head>
+
 <body>
     <div class="flex">
         @include('components.sidebar')
-        <div>
+        <div class="w-full">
             @yield('content')
         </div>
     </div>
+
+    <script src="{{ asset('/app.js') }}"></script>
 </body>
+
 </html>
