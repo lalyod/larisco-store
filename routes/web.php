c4fd66d3a799b2a1a768ef544be3938ee4d041b3
lalyod <mpaproject307@gmail.com>
2023-10-18 14:40:18 +0700

    feat: add auth and carts route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\IndexController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +20,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [MainController::class, "home"])->name("home");
+
+Route::prefix("carts")->group(function (){
+    Route::get("/", [TransactionController::class, "index"])->name("carts.index");
+
+    Route::put("/{transaction}", [TransactionController::class, "update"])->name("carts.update");
+});
+
+Route::prefix("auth")->group(function () {
+    Route::get('/login', [UserController::class, "login"])->name("auth.login.page");
+    Route::get('/register', [UserController::class, 'register'])->name("auth.register.page");
+
+    Route::post("/register", [AuthController::class, "register"])->name("auth.register.store");
+    Route::post("/login", [AuthController::class, "login"])->name("auth.login.store");
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', [IndexController::class, 'index'])->name("admin.index");
+    Route::prefix('categories')->group(function () {
+        Route::get("/", [IndexController::class, 'category'])->name("admin.categories.index");
+        Route::get("/{category}", [CategoryController::class, 'show'])->name("admin.categories.show");
+        Route::get("/{category}/products", [CategoryController::class, 'products'])->name("admin.categories.products");
+
+        Route::post("/", [CategoryController::class, 'store'])->name("admin.categories.store");
+        Route::put("/{category}", [CategoryController::class, "update"])->name("admin.categories.update");
+        Route::delete("/{category}", [CategoryController::class, 'destroy'])->name("admin.categories.destroy");
+    });
+    Route::prefix("products")->group(function () {
+        Route::get("/", [IndexController::class, 'product'])->name("admin.products.index");
+        Route::get("/{product}", [ProductController::class, 'edit'])->name("admin.products.edit");
+
+        Route::post("/", [ProductController::class, "store"])->name("admin.products.store");
+        Route::put("/{product}", [ProductController::class, "update"])->name("admin.products.update");
+    });
+    Route::prefix("transaction")->group(function () {
+        Route::post("/{product}/products", [TransactionController::class, "store"])->name("transaction.store");
+    });
 });
