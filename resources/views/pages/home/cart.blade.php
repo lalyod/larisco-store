c0252f412ec684fbe6d60b5bc22805fe8ad90a38
lalyod <mpaproject307@gmail.com>
2023-10-19 02:12:30 +0700

    refactor: change transaction variable name to cart

--- resources/views/pages/home/cart.blade.php
+++ resources/views/pages/home/cart.blade.php
@@ -6,30 +6,30 @@
             <h3 class="text-2xl font-bold">Cart</h3>
         </div>
         <div class="mt-10">
-            @foreach ($transactions as $transaction)
+            @foreach ($carts as $cart)
                 <div class="bg-white shadow p-4 w-full rounded-lg flex justify-between">
                     <div class="flex gap-4 font-bold">
-                        <img src="{{ Storage::url('public/products/' . $transaction->product->image) }}"
-                            alt="{{ $transaction->product->image }}" class="w-32 h-32 object-contain">
+                        <img src="{{ Storage::url('public/products/' . $cart->product->image) }}"
+                            alt="{{ $cart->product->image }}" class="w-32 h-32 object-contain">
                         <div class="flex flex-col justify-center gap-2">
                             <span class="text-green">Rp.
-                                {{ number_format($transaction->product->price) }} x {{ $transaction->quantity }}</span>
-                            <span class="text-2xl">{{ $transaction->product->name }}</span>
-                            <span>Stock: {{ $transaction->product->stock }}</span>
+                                {{ number_format($cart->product->price) }} x {{ $cart->quantity }}</span>
+                            <span class="text-2xl">{{ $cart->product->name }}</span>
+                            <span>Stock: {{ $cart->product->stock }}</span>
                         </div>
                     </div>
-                    <x-form.group action="{{ route('carts.update', $transaction->id) }}"
+                    <x-form.group action="{{ route('carts.update', $cart->id) }}"
                         class="font-bold flex flex-col justify-center mr-5 gap-5 items-center" method="POST">
                         @method('PUT')
                         <input type="submit" class="border-2 border-black rounded px-2" value="+" name="increase">
-                        <span>{{ $transaction->quantity }}</span>
+                        <span>{{ $cart->quantity }}</span>
                         <input type="submit" class="border-2 border-black rounded px-2" value="-" name="decrease">
                     </x-form.group>
                 </div>
             @endforeach
         </div>
         <div
-            class="bg-white w-full fixed left-0 -z-10 bottom-0 h-50 border w-3/4 right-0 left-72 rounded-lg shadow px-3 py-4">
+            class="bg-white w-full fixed left-0 -z-10 bottom-0 h-50 border right-0 left-72 rounded-lg shadow px-3 py-4" style="width: 75%">
             <div class="flex flex-col gap-2">
                 <div class="flex justify-between">
                     <span>Subtotal</span>
