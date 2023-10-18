d962c44bef87a18b8b6f81240ea330eeea4dec41
lalyod <mpaproject307@gmail.com>
2023-10-18 17:36:24 +0700

    feat: register gate is-admin and is-user

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('is-admin', function (User $user){
+            return $user->role == 'admin';
+        });
+        Gate::define('is-user', function (User $user){
+            return $user->role = "customer";
+        });
     }
 }
