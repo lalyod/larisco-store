7c8741b4c40baaa57e9caa0813303198676079bf
lalyod <mpaproject307@gmail.com>
2023-10-18 14:43:31 +0700

    feat: add category filter, product cart, and product drawer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="mx-10 mt-8">
-    <x-search-bar link="" placeholder="Cari"/>
-</div>
+    <div class="mx-10 mt-8">
+        <x-search-bar link="" placeholder="Cari" />
+        <div class="mt-4">
+            <div class="flex gap-8">
+                <x-home.category-filter :categories=$categories />
+            </div>
+        </div>
+        @if ($errors->any())
+            {{ $errors }}
+        @endif
+        <div class="mt-10">
+            <h3 class="font-bold text-2xl ">Semua Produk</h3>
+            <div class="mt-5 grid gap-10 items-center grid-cols-4">
+                @foreach ($products as $product)
+                    <x-home.product-card :product=$product />
+                    <x-home.product-drawer :product=$product />
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
