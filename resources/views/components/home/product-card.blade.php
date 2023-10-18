3cce38a48eddc783e52abbd0d0079ce11b76929d
lalyod <mpaproject307@gmail.com>
2023-10-18 16:17:00 +0700

    feat: card component for home page

---
+++ resources/views/components/home/product-card.blade.php
@@ -0,0 +1,11 @@
+<div class="card w-56 h-fit bg-base-100 shadow-xl">
+    <figure><img src="{{ Storage::url('public/products/' . $product->image) }}" alt="Shoes" class="w-44 p-3" /></figure>
+    <div class="card-body pt-0">
+        <h2 class="card-title">{{ $product->name }}</h2>
+        <div class="card-actions justify-between flex items-center">
+            <span class="font-bold">{{ number_format($product->price) }}</span>
+            <label for="my-drawer-{{ $product->id }}" class="drawer-button btn btn-neutral text-2xl"
+                onclick="setPrice({{ $product->price }})">+</label>
+        </div>
+    </div>
+</div>
