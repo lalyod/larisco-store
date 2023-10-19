f3f606c90531f346c9500948684d92457cfbd5aa
lalyod <mpaproject307@gmail.com>
2023-10-20 05:15:11 +0700

    feat: add dynamic input id and dynamic label for

--- resources/views/components/form/textbox.blade.php
+++ resources/views/components/form/textbox.blade.php
@@ -1,4 +1,4 @@
 <div class="mb-3 w-full">
-    <label class="label">{{ $label }}</label>
-    <input {{ $attributes->merge(['class' => 'input input-bordered w-full']) }} />
+    <label class="label" for="{{ $attributes['name'] }}">{{ $label }}</label>
+    <input id="{{ $attributes['name'] }}" {{ $attributes->merge(['class' => 'input input-bordered w-full']) }} />
 </div>
