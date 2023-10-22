cb14a900e7bd8f69b0ae14325d848e7428027a77
lalyod <mpaproject307@gmail.com>
2023-10-22 23:22:34 +0700

    refactor: make modal form working

--- resources/views/components/home/settings/modal.blade.php
+++ resources/views/components/home/settings/modal.blade.php
@@ -1,18 +1,22 @@
 <x-modal.index id="setting_address" title="Alamat">
-    <x-form.group>
-        <x-form.selectbox label="Provinsi" id="provinces" onchange="select('provinces','regencies','Kabupaten')">
+    <x-form.group action="{{ route('settings.update.user') }}" method="POST">
+        @method('PUT')
+        <x-form.selectbox label="Provinsi" id="provinces" onchange="select('provinces','regencies','Kabupaten')"
+            name="province">
             <option value="" disabled selected>Pilih Provinsi</option>
             @foreach ($provinces as $province)
                 <option value="{{ $province->id }}">{{ $province->name }}</option>
             @endforeach
         </x-form.selectbox>
-        <x-form.selectbox label="Kabupaten" id="regencies" onchange="select('regencies','districs','Kecamatan')">
+        <x-form.selectbox label="Kabupaten" id="regencies" onchange="select('regencies','districs','Kecamatan')"
+            name="regency">
             <option value="" disabled selected>Pilih Kabupaten</option>
         </x-form.selectbox>
-        <x-form.selectbox label="Kecamatan" id="districs" onchange="select('districs','villages','Desa')">
+        <x-form.selectbox label="Kecamatan" id="districs" onchange="select('districs','villages','Desa')"
+            name="district">
             <option value="" disabled selected>Pilih Kecamatan</option>
         </x-form.selectbox>
-        <x-form.selectbox label="Desa" id="villages">
+        <x-form.selectbox label="Desa" id="villages" name="village">
             <option value="" disabled selected>Pilih Desa</option>
         </x-form.selectbox>
         <hr>
