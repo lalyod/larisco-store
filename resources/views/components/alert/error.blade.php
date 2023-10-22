f72b12e5aafb3e62ce7825cf20c8ca820607d94e
lalyod <mpaproject307@gmail.com>
2023-10-22 23:32:47 +0700

    refactor: make error alert component like notif

--- resources/views/components/alert/error.blade.php
+++ resources/views/components/alert/error.blade.php
@@ -1,7 +1,13 @@
-<div class="alert alert-error">
-    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" style="color: white" fill="none" viewBox="0 0 24 24">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-    </svg>
-    <span class="text-white">{{ $text }}</span>
+<div class="flex flex-col gap-10 fixed z-20 right-10 top-5">
+    @foreach ($errors->all() as $error)
+        <div class="alert alert-error slide-in w-fit">
+            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" style="color: white"
+                fill="none" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span class="text-white">{{ $error }}</span>
+            <button class="btn btn-ghost text-lg text-white">×</button>
+        </div>
+    @endforeach
 </div>
