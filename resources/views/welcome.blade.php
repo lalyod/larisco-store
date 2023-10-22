90805a67d25290492ffb2c8f68a18ff40fcbd456
lalyod <mpaproject307@gmail.com>
2023-10-22 23:09:25 +0700

    fix: make design phone responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,16 @@
 
 @section('content')
     <div class="mx-10 mt-8">
-        <x-search-bar link="" placeholder="Cari" />
+        <x-search-bar link="{{ route('home') }}" placeholder="Cari" name="search" />
         <div class="my-4">
             <div class="flex gap-8">
                 <x-home.category-filter :categories=$categories />
             </div>
         </div>
-        @foreach ($errors->all() as $error)
-            <x-alert.error text="{{ $error }}" />
-        @endforeach
+        <x-alert.error :errors=$errors />
         <div class="mt-10">
             <h3 class="font-bold text-2xl ">Semua Produk</h3>
-            <div class="mt-5 grid gap-10 items-center grid-cols-4">
+            <div class="mt-5 lg:grid gap-10 flex flex-col items-center lg:grid-cols-4">
                 @foreach ($products as $product)
                     <x-home.product-card :product=$product />
                     <x-home.product-drawer :product=$product />
