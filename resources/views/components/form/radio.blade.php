44b4209d0d15e7421542b5b794c859d159a4601b
lalyod <mpaproject307@gmail.com>
2023-10-22 23:29:39 +0700

    feat: add radio input component

---
+++ resources/views/components/form/radio.blade.php
@@ -0,0 +1,4 @@
+<div class="flex items-center gap-3">
+    <input type="radio" id="{{ $attributes['name'] }}" {{ $attributes }} >
+    <label for="{{ $attributes['name'] }}">{{ $label }}</label>
+</div>
