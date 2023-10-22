54c9aad06c682914593c6d2b214033656861a8fe
lalyod <mpaproject307@gmail.com>
2023-10-22 23:15:00 +0700

    feat&fix: refactor and fix error alert

--- resources/views/pages/admin/products/index.blade.php
+++ resources/views/pages/admin/products/index.blade.php
@@ -3,17 +3,16 @@
 @section('content')
     <div class="px-10 mt-10">
         <x-admin.products.modal :categories=$categories />
-        <div class="flex justify-between">
+        <div class="flex justify-between lg:items-center max-sm:flex-col">
             <h3 class="text-2xl font-bold">Produk</h3>
-            <x-modal.trigger text="Tambah" modal="products_add" class="btn-info text-white" />
+            <x-search-bar link="{{ route('admin.products.index') }}" placeholder="Cari produk" name="product" />
         </div>
-        @if ($errors->any())
-            <x-alert.error text="{{ $errors }}" />
-        @endif
+        <x-alert.error :errors=$errors />
         @if (session()->has('success'))
             <x-alert.success text="{{ session()->get('success') }}" />
         @endif
-        <div class="mt-10">
+        <div class="mt-5">
+            <x-modal.trigger text="Tambah" modal="products_add" class="btn-info text-white mb-5" />
             <x-admin.products.table :products=$products />
         </div>
     </div>
