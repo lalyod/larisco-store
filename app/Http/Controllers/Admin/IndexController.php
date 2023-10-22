592c95b5ee85fd9e3a63c3b4fc58ee7e3a2cc87f
lalyod <mpaproject307@gmail.com>
2023-10-22 23:58:21 +0700

    feat: add filter product and user

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\User;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -29,14 +31,39 @@ class IndexController extends Controller
         ]);
     }
 
-    public function product()
+    public function product(Request $request)
     {
-        $products = Product::all();
+        $products = Product::query();
         $categories = Category::all();
+
+        if ($request->has('product')) {
+            $products->where('name', 'LIKE', '%' . $request->input('product') . '%');
+        }
+
+        $products = $products->get();
+
         return view("pages.admin.products.index", [
             "title" => "products",
             "products" => $products,
             "categories" => $categories
+        ]);
+    }
+
+    public function user(Request $request): View
+    {
+        $users = User::query();
+
+        if ($request->has('role')) {
+            $users->where('role', $request->input('role'));
+        } else if ($request->has('user')) {
+            $users->where('name', 'LIKE', '%' . $request->input('user') . '%');
+        }
+
+        $users = $users->get();
+
+        return view('pages.admin.users.index', [
+            'title' => 'users',
+            'users' => $users
         ]);
     }
 }
