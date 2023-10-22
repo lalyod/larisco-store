1a3064441947445abc37aa70bc2e3bd4d4ff50ba
lalyod <mpaproject307@gmail.com>
2023-10-22 23:32:13 +0700

    refactor: make success component like notif

--- resources/views/components/alert/success.blade.php
+++ resources/views/components/alert/success.blade.php
@@ -1,7 +1,9 @@
-<div class="alert alert-success mt-5">
-    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+<div class="alert alert-success slide-in fixed z-20 w-fit right-10 top-5">
+    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" style="color: white" fill="none"
+        viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
-    <span class="text-white">{{ $text }}</span>
+    <span class="text-white text-lg">{{ $text }}</span>
+    <button class="btn btn-ghost text-lg text-white">×</button>
 </div>
