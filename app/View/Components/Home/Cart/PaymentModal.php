cbd52032991fd2e28314eb93ad532c142d16a78c
lalyod <mpaproject307@gmail.com>
2023-10-22 23:45:44 +0700

    feat: add payment modal

---
+++ app/View/Components/Home/Cart/PaymentModal.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\View\Components\Home\Cart;
+
+use Illuminate\View\Component;
+
+class PaymentModal extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        public $user,
+        public $subtotal
+    )
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.home.cart.payment-modal');
+    }
+}
