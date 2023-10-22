2b76f15059a8a649e0592a6a0a8568460e920f50
lalyod <mpaproject307@gmail.com>
2023-10-22 23:30:36 +0700

    feat: create date input component

---
+++ resources/views/components/form/datebox.blade.php
@@ -0,0 +1,4 @@
+<div class="w-full">
+    <label for="{{ $attributes['name'] }}" class="label">{{ $label }}</label>
+    <input type="date" id="{{ $attributes['name'] }}" class="input input-bordered w-full" {{ $attributes }}>
+</div>
