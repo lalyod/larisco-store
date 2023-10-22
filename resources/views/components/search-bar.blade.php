1db1d3bed20a7e7b8051eca18f717fcfd4c03bdb
lalyod <mpaproject307@gmail.com>
2023-10-22 23:39:32 +0700

    refactor: responsive phone size

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -1,5 +1,5 @@
-<form action="{{ $link }}" method="GET" class="flex gap-2">
-    <input {{ $attributes }} class="input input-bordered w-full max-w-md" />
+<form action="{{ $link }}" method="GET" class="flex gap-2 max-sm:mt-5">
+    <input {{ $attributes }} class="input input-bordered w-full max-w-lg" />
     <button class="btn btn-neutral">
         <img src="{{ asset('/img/search.svg') }}" alt="" class="filter-white">
     </button>
