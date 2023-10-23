ff13717284f120dafc932f8d1763c1f6288ead4f
lalyod <mpaproject307@gmail.com>
2023-10-23 12:40:52 +0700

    feat: total cart component

---
+++ app/View/Components/Home/Cart/Total.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Components\Home\Cart;
+
+use Illuminate\View\Component;
+
+class Total extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        public int $subtotal
+    ) {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.home.cart.total');
+    }
+}
