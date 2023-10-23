e1efb5b6845599a1008ace695cbe10e8a296a680
lalyod <mpaproject307@gmail.com>
2023-10-23 12:38:18 +0700

    feat: add auto close nad make close button work

--- resources/views/components/alert/success.blade.php
+++ resources/views/components/alert/success.blade.php
@@ -1,9 +1,9 @@
-<div class="alert alert-success slide-in fixed z-20 w-fit right-10 top-5">
+<div class="alert alert-success max-sm:flex slide-in fixed z-20 w-fit lg:right-10 top-5" id="alert-success">
     <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" style="color: white" fill="none"
         viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
     <span class="text-white text-lg">{{ $text }}</span>
-    <button class="btn btn-ghost text-lg text-white">×</button>
+    <button class="btn btn-ghost text-lg text-white" onclick="buttonHideAlertSuccess()">×</button>
 </div>
